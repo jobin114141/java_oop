fc5e4081883feabca99fc48f7b5ab025cf0b6076
jobin george <94526813+jobin114141@users.noreply.github.com>
2022-04-11 07:04:53 +0530

    Update ComplexNum.java

--- CO1/ComplexNum.java
+++ CO1/ComplexNum.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package nimisha.tony;
+package jobin.george;
 
 import java.util.Scanner;
 
 /**
  *
- * @author Nimisha
+ * @author Jobin
  */
 public class ComplexNum {
     int real,img;
